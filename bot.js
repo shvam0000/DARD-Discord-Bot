811b0728f7c9fbebd5ea7001fe6e0eb8dd3edbf9
Shivam Shekhar <shvam0000@gmail.com>
2021-01-16 12:23:33 +0530

    (fix): nodemon start for the bot

--- bot.js
+++ bot.js
@@ -1,34 +1,34 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const discord = require('discord.js');
 const client = new discord.Client({
-    partials: ["MESSAGE"]
-})
+  partials: ['MESSAGE'],
+});
 
 const botPrefix = '!';
 const modMeCommand = 'mod-me';
 
 client.on('ready', () => {
-    console.log('Our bot is ready to go');
-})
+  console.log('Our bot is ready to go');
+});
 
-client.on('messageDelete', msg => {
-    msg.channel.send(`You deleted a message, you're sus bruh`);
-})
+client.on('messageDelete', (msg) => {
+  msg.channel.send(`You deleted a message, you're sus bruh`);
+});
 
-client.on('message', msg => {
-    if (msg.content === 'I love DARD') {
-        msg.react('💔');
-        msg.reply(`You're a DARD person! 💔`);
-    }
-    if (msg.content === `${botPrefix}${modMeCommand}`) {
-        modUser(msg.member)
-    }
-})
+client.on('message', (msg) => {
+  if (msg.content === 'I love DARD') {
+    msg.react('💔');
+    msg.reply(`You're a DARD person! 💔`);
+  }
+  if (msg.content === `${botPrefix}${modMeCommand}`) {
+    modUser(msg.member);
+  }
+});
 
 function modUser(member) {
-    member.roles.add("784404727110041630");
-    msg.reply(`You're a Moderator now, play safe!`);
+  member.roles.add('784405929483173948');
+  msg.reply(`You're a Moderator now, play safe!`);
 }
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
